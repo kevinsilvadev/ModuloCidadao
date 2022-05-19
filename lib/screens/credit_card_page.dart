bcf19d8250af0478dcd7de9215f3cfa7932bcbf8
Emílio Biasi <emiliobiasi@Emilios-MacBook-Air.local>
2022-05-19 18:36:05 -0300

    Navegacao das telas

---
+++ lib/screens/credit_card_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class credit_card_page extends StatefulWidget {
+  const credit_card_page({Key? key}) : super(key: key);
+
+  @override
+  _credit_card_pageState createState() => _credit_card_pageState();
+}
+
+class _credit_card_pageState extends State<credit_card_page> {
+
+  //Código daqui pra baixo
+
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+
+  );
+
+}

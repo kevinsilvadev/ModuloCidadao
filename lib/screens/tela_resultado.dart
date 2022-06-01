67fff657f14108e3c4fff85e9b98ddac17f4a270
Emílio Biasi <ejbiasi@hotmail.com>
2022-06-01 19:01:43 -0300

    card de sucesso na compra

---
+++ lib/screens/tela_resultado.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class tela_resultado extends StatefulWidget {
+  const tela_resultado({Key? key}) : super(key: key);
+
+  @override
+  _tela_resultadoState createState() => _tela_resultadoState();
+}
+
+class _tela_resultadoState extends State<tela_resultado> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+
+    );
+  }
+}

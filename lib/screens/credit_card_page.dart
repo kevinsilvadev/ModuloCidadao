011e50aa1d18d4428a80bf1ffa41ae07cd449ad1
Emílio Biasi <ejbiasi@hotmail.com>
2022-06-05 00:12:14 -0300

    Mega ultra blaster incomparavel inconfuldivel e acima de tudo UNBELIEVABLE Update

--- lib/screens/credit_card_page.dart
+++ lib/screens/credit_card_page.dart
@@ -3,14 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:modulo_cidadao/main.dart';
 import 'package:modulo_cidadao/screens/maps.dart';
+import 'package:modulo_cidadao/screens/metodosPagamento.dart';
 
 
 class credit_card_page extends StatefulWidget {
 
+
+  List zonaAzulComerciosNomes;
+  List zonaAzulComerciosNomes2;
   int _counter;
   int valorDoTicket;
   String placaDoVeiculo;
-  credit_card_page(this.valorDoTicket, this._counter, this.placaDoVeiculo);
+  credit_card_page(this.valorDoTicket, this._counter, this.placaDoVeiculo, this.zonaAzulComerciosNomes, this.zonaAzulComerciosNomes2);
 
   @override
   _credit_card_pageState createState() => _credit_card_pageState();
@@ -24,6 +28,8 @@ class _credit_card_pageState extends State<credit_card_page> {
   String cardHolderName = '';
   String cvvCode = '';
   bool isCvvFocused = false;
+  String paymentResult = '';
+
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
@@ -39,8 +45,12 @@ class _credit_card_pageState extends State<credit_card_page> {
         'hoursAcquired': widget._counter,
         'amount': widget.valorDoTicket,
       });
-      print("result: ${resp.data}");
+      paymentResult = resp.data["status"];
+      print(resp.data["status"]);
+      return resp.data["status"];
     }
+
+
     return Scaffold(
       backgroundColor: Colors.teal[50],
       appBar: AppBar(
@@ -111,26 +121,92 @@ class _credit_card_pageState extends State<credit_card_page> {
                         onPressed: (){
                           if(formKey.currentState!.validate()){
                             payment();
-                            print('valid');
-                            final AlertDialog infoTicket = AlertDialog(
-                              title: Text('Pagamento Efetuado!',
-                              textAlign: TextAlign.center,),
-                              //chamar infos do banco da área aqui
-                              content: Text('Seu ticket da \$ foi comprado com SUCESSO!\nO prazo de estacionamento é de ${widget._counter} horas.\nValor do ticket: R\$ 0${widget.valorDoTicket},00',
-                              textAlign: TextAlign.center),
-                              actions: [
-                                FlatButton(
-                                  textColor: Color(0xFF6200EE),
-                                  onPressed: () {
-                                    Navigator.of(context).push(
-                                      MaterialPageRoute(builder: (context) => maps(polylineList, polylineList2, zonaAzulComercios, zonaAzulComercios2, zonaAzulComerciosNomes,zonaAzulComerciosNomes2)),
-                                    );
-                                  },
-                                  child: Text('OK'),
-                                ),
-                              ],
-                            );
-                            showDialog(context: context, builder: (context) => infoTicket);
+                            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                              content: Text('Processando ...'),
+                            ));
+                            Future.delayed(const Duration(seconds: 3), () {
+                              print(paymentResult);
+                              print('valid');
+                              if (paymentResult == 'TRANSACAO_EFETUADA'){
+                                final AlertDialog infoTicket = AlertDialog(
+                                  title: Text('Pagamento Efetuado!',
+                                    textAlign: TextAlign.center,),
+                                  //chamar infos do banco da área aqui
+                                  content: Text('Ticket da área comprado com SUCESSO!\nO prazo de estacionamento é de ${widget._counter} horas.\nValor do ticket: R\$ 0${widget.valorDoTicket},00',
+                                      textAlign: TextAlign.center),
+                                  actions: [
+                                    FlatButton(
+                                      textColor: Color(0xFF6200EE),
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(builder: (context) => maps(polylineList, polylineList2, zonaAzulComercios, zonaAzulComercios2, zonaAzulComerciosNomes,zonaAzulComerciosNomes2)),
+                                        );
+                                      },
+                                      child: Text('OK'),
+                                    ),
+                                  ],
+                                );
+                                showDialog(context: context, builder: (context) => infoTicket);
+                              } else if (paymentResult == 'CARTAO_INVALIDO'){
+                                final AlertDialog infoTicketFalha = AlertDialog(
+                                  title: Text('Transação NÃO Efetuada!',
+                                    textAlign: TextAlign.center,),
+                                  //chamar infos do banco da área aqui
+                                  content: Text('O ticket não pode ser adquirido, pois o cartão digitado não é válido.',
+                                      textAlign: TextAlign.center),
+                                  actions: [
+                                    FlatButton(
+                                      textColor: Color(0xFF6200EE),
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(builder: (context) => metodosPagamento(widget.valorDoTicket, widget._counter, widget.placaDoVeiculo, widget.zonaAzulComerciosNomes, widget.zonaAzulComerciosNomes2)),
+                                        );
+                                      },
+                                      child: Text('OK'),
+                                    ),
+                                    FlatButton(
+                                      textColor: Color(0xFF6200EE),
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(builder: (context) => maps(polylineList, polylineList2, zonaAzulComercios, zonaAzulComercios2, zonaAzulComerciosNomes,zonaAzulComerciosNomes2)),
+                                        );
+                                      },
+                                      child: Text('CANCELAR COMPRA'),
+                                    ),
+                                  ],
+                                );
+                                showDialog(context: context, builder: (context) => infoTicketFalha);
+                              } else if (paymentResult == 'TRANSACAO_NAO_AUTORIZADA') {
+                                final AlertDialog infoTicketFalha = AlertDialog(
+                                  title: Text('Transação NÃO Autorizada!',
+                                    textAlign: TextAlign.center,),
+                                  //chamar infos do banco da área aqui
+                                  content: Text('O ticket não pode ser adquirido, pois a transação do pagamento não pode ser efetuada com sucesso.',
+                                      textAlign: TextAlign.center),
+                                  actions: [
+                                    FlatButton(
+                                      textColor: Color(0xFF6200EE),
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(builder: (context) => metodosPagamento(widget.valorDoTicket, widget._counter, widget.placaDoVeiculo, widget.zonaAzulComerciosNomes, widget.zonaAzulComerciosNomes2)),
+                                        );
+                                      },
+                                      child: Text('OK'),
+                                    ),
+                                    FlatButton(
+                                      textColor: Color(0xFF6200EE),
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(builder: (context) => maps(polylineList, polylineList2, zonaAzulComercios, zonaAzulComercios2, zonaAzulComerciosNomes,zonaAzulComerciosNomes2)),
+                                        );
+                                      },
+                                      child: Text('CANCELAR COMPRA'),
+                                    ),
+                                  ],
+                                );
+                                showDialog(context: context, builder: (context) => infoTicketFalha);
+                              }
+                            });
                           }
                           else{
                             print('inValid');
